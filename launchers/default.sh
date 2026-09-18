be382a1675a35623a8e0710cf1a94166025f8ab5
Joe Guymon <jg737@byu.edu>
2026-09-18 16:29:05 -0600

    lab1 half way

--- launchers/default.sh
+++ launchers/default.sh
@@ -8,12 +8,11 @@ dt-launchfile-init
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-
 # NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+dt-exec python3 -m "my_package.my_script"
 
 
 # ----------------------------------------------------------------------------
---
+++ packages/my_package/my_script.py
@@ -0,0 +1,5 @@
+import os
+
+vehicle_name = os.environ['VEHICLE_NAME']
+message = f"\nHello from leia!\n"
+print(message)
